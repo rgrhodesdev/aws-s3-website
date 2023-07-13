b91d00521a89f1f1a1722e39f54b1465950aec49
Richard Rhodes <rgrhodesdev@gmail.com>
2023-07-13 21:59:37 +0100

    updates

--- main.tf
+++ main.tf
@@ -4,19 +4,41 @@ resource "aws_s3_bucket" "mywebsite" {
 
 resource "aws_s3_bucket_website_configuration" "mywebsite-config" {
   bucket = aws_s3_bucket.mywebsite.bucket
-  index_document = {
+
+  index_document {
     suffix = "index.html"
   }
+  
 }
 
 resource "aws_s3_bucket_policy" "mywebsite-policy" {
   bucket = aws_s3_bucket.mywebsite.id
   policy = templatefile("mywebsite-s3-policy.json", { bucket = var.bucketName })
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+  
 }
 
 resource "aws_s3_object" "mywebsite-index" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.mywebsite.id
   key    = "index.html"
-  source = "../src/index.html"
+  source = "src/index.html"
   acl    = "public-read"
+  depends_on = [aws_s3_bucket_policy.mywebsite-policy]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.mywebsite.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mywebsite.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
